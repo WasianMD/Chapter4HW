16d40025ba8ddf48feed3a7b05a94cca7e5967ce
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-10-18 23:23:14 -0400

    Create RandomArray.java

---
+++ RandomArray.java
@@ -0,0 +1,18 @@
+public class RandomArray {
+    public static void main(String[] args) {
+        double[] random = {};
+        for (int i = 0; i < 50; i++) {
+         random[i] = rd.nextDouble(); 
+      }
+        java.util.Scanner input = new java.util.Scanner(System.in);
+            try {
+                System.out.println("Enter a number between 1 and 50 and see what number you get: ");
+                int index = input.nextInt();
+                System.out.println(random[lastIndex]);
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+                e.printStackTrace();
+                input.nextLine();
+            }
+    }
+}
